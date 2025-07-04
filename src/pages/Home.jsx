0c0087e9be047c0027a25fd195364f59709d6f68
Muhammad Dean Fahreza <muhammaddean27@gmail.com>
2025-07-04 19:43:13 +0700

    add: Menambahkan file ke dalam repositori

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
-import { StarBackground } from "@/components/StarBackground"
+import { StarBackground } from "../components/StarBackground"
 import { ThemeToggle } from "../components/ThemeToggle"
+import { Navbar } from "../components/Navbar"
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -10,8 +11,7 @@ export const Home = () => {
         <StarBackground />
 
         {/** Navbar (Navigasi) */}
-
-        {/** Navbar */}
+        <Navbar />
 
         {/** Main Content (konten utama) */}
 
