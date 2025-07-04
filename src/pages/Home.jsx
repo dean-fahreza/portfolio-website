73240d8c6455b42f9fc8d6643fbb65d998c6c4e5
Muhammad Dean Fahreza <muhammaddean27@gmail.com>
2025-07-04 10:33:35 +0700

    add: Menambahkan file ke dalam repositori

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import { StarBackground } from "@/components/StarBackground"
+import { ThemeToggle } from "../components/ThemeToggle"
+
+export const Home = () => {
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+        {/** Theme toggle atau tombol tema */}
+        <ThemeToggle />
+
+        {/** Background Effects (Efek latar) */}
+        <StarBackground />
+
+        {/** Navbar (Navigasi) */}
+
+        {/** Navbar */}
+
+        {/** Main Content (konten utama) */}
+
+        {/** Footer */}
+
+    </div>
+}
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+export const NotFound = () => {
+  return (
+    <div className="home">
+      Not Found 404
+    </div>
+  );
+}
